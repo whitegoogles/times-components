e9bdb41f0de547fa5fd6e82dc807e3c0f9278896
James Miller <james@james-miller.co.uk>
2018-07-10 11:52:41 +0100

    chore: Added required interactives

--- packages/interactive/src/interactive.js
+++ packages/interactive/src/interactive.js
@@ -8,7 +8,7 @@ const htmlBuilder = ({attributes, element, source}) => {
 <html>
   <head><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></head>
   <body>
-      <link rel="import" href="${source}" />
+      <link rel="import" href="https:${source}" />
       <${element} ${elementAttributes}></${element}>
       <script>
           (function () {
